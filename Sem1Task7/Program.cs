1ddda6556b24019366bb984d0cac9e184289c67a
Vera <tarasevich28_88@mail.ru>
2022-09-01 11:14:35 +0300

    Добавление

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿//===========================================
+//# 7 Напишите программу, которая на вход принимает трехзначное число, 
+//а на выходе показывает последнюю цифру. 
+//==========================================
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    if (inputNumber > 99 && inputNumber < 1000)
+    {
+        int lastDigit = inputNumber % 10;
+        Console.Write(lastDigit + " ");
+    }
+}
