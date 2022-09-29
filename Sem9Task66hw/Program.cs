36d1426b4e8044a3f6961200488e0a529b36e35f
Vera <tarasevich28_88@mail.ru>
2022-09-29 22:42:52 +0300

    Семинар про рекурсию

---
+++ Sem9Task66hw/Program.cs
@@ -0,0 +1,44 @@
+﻿//  66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 4; N = 8. -> 30
+//-----------------
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+int RecSumMN(int M, int N)
+{
+    if (M >= N) return N;
+    
+        int sum = M + RecSumMN(M + 1, N);
+    
+    return sum;
+}
+
+bool TestNum(int m, int n)
+{
+    bool num = false;
+    if (m < n) num = true;
+    else num = false;
+    return num;
+}
+
+int m = ReadData("Введите числа M: ");
+int n = ReadData("Введите числа N: ");
+
+// Вывод данных
+void PrintData(int m, int n)
+{
+    if (TestNum(m, n))
+    {
+        Console.WriteLine(RecSumMN(m, n));
+    }
+    else
+    {
+         Console.WriteLine(RecSumMN(n, m));
+    }
+}
+PrintData(m,n);
---
+++ Sem9Task68hw/Program.cs
@@ -0,0 +1,37 @@
+﻿//  Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9 | m = 3, n = 2 -> A(m,n) = 29
+//-----------------------------------------------------------------------
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+//Печать в консоль
+void PrintResult(int line)
+{
+    Console.WriteLine(line);
+}
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    if (m != 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+PrintResult(Akkerman(n, m));
